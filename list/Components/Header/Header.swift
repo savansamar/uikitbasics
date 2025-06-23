fe24af91370b54f1491ffca820206a8ca5c611d7
savansamar <savansoni125@gmail.com>
2025-06-23 10:30:39 +0530

    first commit

---
+++ list/Components/Header/Header.swift
@@ -0,0 +1,20 @@
+//
+//  Header.swift
+//  list
+//
+//  Created by MACM72 on 20/06/25.
+//
+
+import UIKit
+
+class Header: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
